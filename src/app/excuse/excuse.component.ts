fac109bdc871dd3e29944c40ec2ac9ca8598d45b
rayonhunte <rayon.hunte@gmail.com>
2021-09-15 09:11:50 -0400

    updates

---
+++ src/app/excuse/excuse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excuse',
+  templateUrl: './excuse.component.html',
+  styleUrls: ['./excuse.component.css']
+})
+export class ExcuseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
