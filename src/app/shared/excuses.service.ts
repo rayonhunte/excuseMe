9b187e3d3e11e6a951494679aa1dcace1b1e99bd
rayonhunte <rayon.hunte@gmail.com>
2021-09-23 16:29:31 -0400

    updates

--- src/app/shared/excuses.service.ts
+++ src/app/shared/excuses.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, addDoc, getFirestore } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
 
 
@@ -9,12 +9,25 @@ import { Observable, of } from 'rxjs';
 export class ExcusesService {
 
 
+
   constructor(private fireStore: Firestore) { }
 
 
   getExcuses(): Observable<any> {
     return collectionData(collection(this.fireStore, 'excuses'))
   }
+
+  async addExcuse(excuse: {
+    excuse: {
+      intro: string,
+      goat: string,
+      delay: string,
+    }
+  }) {
+    const db = getFirestore()
+    const docRef = await addDoc(collection(this.fireStore, 'excuses'), excuse)
+  }
+
 }
 
 
